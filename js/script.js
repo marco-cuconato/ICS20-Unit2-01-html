9dc5d01f9f2a113b9993a682c910f7c86f7703b7
marco-cuconato <99355087+marco-cuconato@users.noreply.github.com>
2022-03-10 18:26:05 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+// Created by: marco cuconato 
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  // this function displays "Hello, World!"
+
+  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>";
+}
